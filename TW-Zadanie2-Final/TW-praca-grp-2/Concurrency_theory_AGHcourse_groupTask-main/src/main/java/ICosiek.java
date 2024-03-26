92d124877d3ee2208c3b8e9cf2bea06be9904508
dominiks01 <dominikszot950@gmail>
2024-03-26 14:02:06 +0100

    cleanup

--- TW-Zadanie2-Final/TW-praca-grp-2/Concurrency_theory_AGHcourse_groupTask-main/src/main/java/ICosiek.java
+++
@@ -1,5 +0,0 @@
-public interface ICosiek {
-    void consume(int idx, int portion) throws InterruptedException;
-    void produce(int idx, int portion) throws InterruptedException;
-    void printAmount();
-}
